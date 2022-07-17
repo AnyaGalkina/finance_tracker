4f3c80942199fb02e428c01c7b154158b6e21a71
AnyaGalkina <anna.blackbird1@gmail.com>
2022-07-17 16:15:24 +0700

    Add file with categories IDs

---
+++ src/reducers/categoryId.ts
@@ -0,0 +1,10 @@
+import {v1} from "uuid";
+
+export const transportId =  v1();
+export const foodId =  v1();
+export const travelId =  v1();
+export const petId =  v1();
+export const houseId =  v1();
+export const healthCareId =  v1();
+export const babyId =  v1();
+export const clothesId =  v1();
