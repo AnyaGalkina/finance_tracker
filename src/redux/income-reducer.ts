99e38587e6a2b0f48646897ebdae129ad94d3266
AnyaGalkina <anna.blackbird1@gmail.com>
2022-07-20 00:10:47 +0700

    Add income and totalIncome Reducers

---
+++ src/redux/income-reducer.ts
@@ -0,0 +1,62 @@
+import {CHANGE_CATEGORY_NAME, ChangeCategoryNameType, SvgComponentType} from "./totalSpends-reducer";
+import {SumType} from "./spend-reducer";
+import {v1} from "uuid";
+
+const ADD_INCOME = "ADD_INCOME";
+const REMOVE_INCOME = "REMOVE_INCOME";
+
+export type AddIncomeType = ReturnType<typeof addIncomeAC>;
+export type RemoveIncomeType = ReturnType<typeof removeIncomeAC>;
+
+
+type ActionType = AddIncomeType | RemoveIncomeType | ChangeCategoryNameType;
+
+export const initialState: SumType[] = [];
+
+
+export const incomeReducer = (state: SumType[] = initialState, action: ActionType): SumType[] => {
+    switch (action.type) {
+        case ADD_INCOME:
+            let newIncome = {
+                categoryId: action.payload.categoryId,
+                id: action.payload.id,
+                categoryName: action.payload.categoryName,
+                sum: action.payload.spend,
+                component: action.payload.component
+            }
+            return [...state, newIncome];
+        case CHANGE_CATEGORY_NAME:
+            debugger
+            return state.map(s =>
+                s.categoryId === action.payload.categoryId ? {...s, categoryName: action.payload.categoryName} : s
+            );
+        case REMOVE_INCOME:
+            return state.filter(s => s.id !== action.payload.id)
+        default:
+            return state;
+    }
+}
+
+export const addIncomeAC = (categoryId: string, categoryName: string, spend: number, component: SvgComponentType) => {
+    return {
+        type: ADD_INCOME,
+        payload: {
+            categoryId,
+            id: v1(),
+            categoryName,
+            spend,
+            component
+        }
+    } as const
+}
+
+
+export const removeIncomeAC = (id: string) => {
+    return {
+        type: REMOVE_INCOME,
+        payload: {
+            id
+        }
+    } as const
+}
+
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,16 @@
 import {combineReducers, createStore} from "redux";
 import {spendsReducer} from "./spend-reducer";
 import {totalSpendReducer} from "./totalSpends-reducer";
+import {totalIncomeReducer} from "./totalIncome-reducer";
+import {incomeReducer} from "./income-reducer";
 
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
 const rootReducer = combineReducers({
     spends: spendsReducer,
-    totalSpends: totalSpendReducer
+    totalSpends: totalSpendReducer,
+    income: incomeReducer,
+    totalIncome: totalIncomeReducer,
 });
 
 export const store = createStore(rootReducer);
