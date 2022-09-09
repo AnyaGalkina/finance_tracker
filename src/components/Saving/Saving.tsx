b0e69f9ee583147a9183fd28245f548d19f99195
AnyaGalkina <anna.blackbird1@gmail.com>
2022-09-09 22:11:29 +0300

    Add savings placeholder

---
+++ src/components/Saving/Saving.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "../CommonInput/CommonInput.module.css";
+
+const Saving = () => {
+    return (
+        <div>
+            <div>
+                <span>Choose percent from any income</span>
+            </div>
+        </div>
+    );
+};
+
+export default Saving;
