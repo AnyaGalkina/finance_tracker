4c045f0a6490daebee5e16e5e08a0ea9734ec1bb
Anna Galkina <anna.blackbird1@gmail.com>
2023-02-16 18:26:21 +0700

    Updates

--- service-worker.ts
+++ service-worker.ts
@@ -1,11 +1,4 @@
-import { clientsClaim } from 'workbox-core';
-import { ExpirationPlugin } from 'workbox-expiration';
-import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
-import { registerRoute } from 'workbox-routing';
-import { StaleWhileRevalidate } from 'workbox-strategies';
-
-
-// declare const self: ServiceWorkerGlobalScope;
+import React from 'react';
 
 /* eslint-disable-next-line no-restricted-globals */
 self.addEventListener('install', async  (event: any) => {
